931b20f24d4a4bed50b6584af279abcb7f0c4bbd
kengste <koh.kenghong@outlook.com>
2017-11-12 10:56:30 +0800

    add import reducers

--- server/src/client/reducers/index.js
+++ server/src/client/reducers/index.js
@@ -1,6 +1,6 @@
-import { combinedReducers } from 'redux';
+import { combineReducers } from 'redux';
 import usersReducer from './usersReducer';
 
-export default combinedReducers({
+export default combineReducers({
     users: usersReducer
 })
